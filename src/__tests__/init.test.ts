8f229ff3b81ae92bb849af8d79e09c023c6f73f9
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-05-29 04:44:24 +0100

    1.0.3

---
+++ src/__tests__/init.test.ts
@@ -0,0 +1,8 @@
+import { labelIsCIP68 } from '../index';
+
+test('Label is CIP68', () => {
+  expect(labelIsCIP68(31337)).toBe(false);
+  expect(labelIsCIP68(100)).toBe(true);
+  expect(labelIsCIP68(222)).toBe(true);
+  expect(labelIsCIP68(223)).toBe(false);
+});
