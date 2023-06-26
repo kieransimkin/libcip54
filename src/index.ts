4089dde8af931d7b6fa5c98ff8810f18a5d7749f
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-06-26 02:53:25 +0100

    1.0.46

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import pJSON from '../package.json';
-import * as Libcip54 from "./libcip54.js"
+import * as Libcip54 from './libcip54.js';
 const version = pJSON.version;
 const getVersion = () => {
   return pJSON.version || '0';
 };
-module.exports = {...Libcip54, version, getVersion}
+module.exports = { ...Libcip54, version, getVersion };
