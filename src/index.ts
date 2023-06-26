451f35c303015a63b909492842d8b6b3e9a1bfd7
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-06-26 02:58:05 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import pJSON from '../package.json';
-import * as Libcip54 from './libcip54.js';
-const version = pJSON.version;
-const getVersion = () => {
+export const version = pJSON.version;
+export const getVersion = () => {
   return pJSON.version || '0';
 };
-module.exports = { ...Libcip54, version, getVersion };
+export * from "./libcip54"
