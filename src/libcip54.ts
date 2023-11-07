3b063e3de2ceefd800afa2821c128a3435424bba
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-07 08:24:10 +0000

    tidy

--- src/libcip54.ts
+++ src/libcip54.ts
@@ -1200,9 +1200,9 @@ const base64ToUnicode = (str: string) => {
 };
 function hexToAscii(str1: string)
  {
-	var hex  = str1.toString();
-	var str = '';
-	for (var n = 0; n < hex.length; n += 2) {
+	const hex  = str1.toString();
+  let str = '';
+	for (let n = 0; n < hex.length; n += 2) {
 		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
 	}
 	return str;
