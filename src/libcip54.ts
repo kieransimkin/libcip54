48842751fd046e26b17a7ca15328491d38bec7c6
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-07 07:59:55 +0000

    unicode support

--- src/libcip54.ts
+++ src/libcip54.ts
@@ -1193,7 +1193,7 @@ const base64ToUnicode = (str:string) => {
   return decodeURIComponent(
       atob(str)
           .split('')
-          .map(function (c) {
+          .map( (c) => {
               return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
           })
           .join('')
