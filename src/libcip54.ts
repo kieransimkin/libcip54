9d59337e2e515c667fa92da9b4ae35c2f57e6b1c
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 23:31:55 +0000

    fix

--- src/libcip54.ts
+++ src/libcip54.ts
@@ -385,11 +385,11 @@ export const getMetadata = async (unit: string): Promise<any> => {
   if (!label || !labelIsCIP68(label)) {
     const mintTx = await getMintTx(unit);
     if (mintTx && mintTx.metadata && typeof mintTx.metadata === 'object') {
-      const nftMetadata: any = mintTx.metadata.filter((m: any) => m.key === 721)[0].json;
-      const policyMetadata = nftMetadata[policyId];
-      if (policyMetadata[Buffer.from(assetName || '', 'hex').toString()]) {
+      const nftMetadata: any = mintTx.metadata.filter((m: any) => m.key === CIP25_LABEL)[0]?.json;
+      const policyMetadata = nftMetadata ? nftMetadata[policyId] : null;
+      if (policyMetadata && policyMetadata[Buffer.from(assetName || '', 'hex').toString()]) {
         metadata = policyMetadata[Buffer.from(assetName || '', 'hex').toString()];
-      } else if (policyMetadata[assetName || '']) {
+      } else if (policyMetadata && policyMetadata[assetName || '']) {
         metadata = policyMetadata[assetName || ''];
       } else {
         metadata = null;
